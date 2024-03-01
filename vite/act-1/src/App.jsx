4d950ee9908b5d7e94dde502c7107829d8225638
Gherlan L. Aires <gherlan.aires@outlook.com>
2024-03-01 17:15:34 -0300

    att

--- vite/act-1/src/App.jsx
+++ vite/act-1/src/App.jsx
@@ -1,34 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import './styles/App.css'
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <>
+          <img src="../public/images/react.png" alt="Logo do React.js" id="react-logo"/>
+          <h1>React.js</h1>
+
+          <button>Aprenda React</button>
+          <button>Referência de API</button>
+      </>
   )
 }
 
---
+++ vite/fisrt-react-project/src/App.jsx
@@ -0,0 +1,7 @@
+export default function App(){
+  return (
+    <div>
+      <h1>Oi, bebê</h1>
+    </div>
+  )
+}
